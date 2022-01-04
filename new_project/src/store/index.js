f56cc3b1eeeca43784ffce86dfb2cbab0b8998eb
Fhommand <85489785+Fhommand@users.noreply.github.com>
2022-01-04 15:59:57 +0100

    call api all characters

--- new_project/src/store/index.js
+++ new_project/src/store/index.js
@@ -1,33 +1,42 @@
 import { createStore } from 'vuex'
 import axios from 'axios'
 
-
 export default createStore({
-
- state : {
-  infos: [],
-  characters: [],
-},
-
- getters : {},
-
- actions : {
-  getDatas({ commit }) {
-      axios.get('https://rickandmortyapi.com/api/character')
-          .then(response => {
-              commit('SET_INFOS', response.data)
-              commit('SET_CHARACTERS', response.data.results)
-      })
-  }
-},
-
- mutations : {
-  SET_INFOS(state, infos) {
-      state.infos = infos
+  state: {
+    infos: [],
+    characters: []
   },
-  SET_CHARACTERS(state, characters) {
-    state.characters = characters
-}
-},
 
+  getters: {},
+
+  actions: {
+    getDatas ({ commit }) {
+        
+        async function callUrl(url) {
+        
+            await axios.get(url).then(response => {
+
+                commit('SET_INFOS', response.data.info)
+                commit('SET_CHARACTERS', response.data.results)
+
+                if (response.data.info.next != null) {
+                    return callUrl(response.data.info.next);
+                }
+            })
+        }
+        
+        
+        let url = 'https://rickandmortyapi.com/api/character';
+        callUrl(url);
+    }
+  },
+
+  mutations: {
+    SET_INFOS (state, infos) {
+      state.infos = infos
+    },
+    SET_CHARACTERS (state, characters) {
+      state.characters.push(...characters);
+    }
+  }
 })
